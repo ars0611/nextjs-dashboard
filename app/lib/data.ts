2460255ca4a61f3c3b1109afb6bcd75acbdb55fd
ars0611 <cfky1251012@gn.iwasaki.ac.jp>
2025-08-27 23:21:53 +0900

    section6~10

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -58,10 +58,11 @@ export async function fetchCardData() {
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
-         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-         FROM invoices`;
+      SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
+      SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+      FROM invoices`;
 
+      //ここで並列に実行しているらしい
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
